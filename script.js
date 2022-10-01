6b6c897b66d55b8b12abf2a43a0182fd793c9066
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-01 12:54:40 -0500

    Added a current time variable

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var currentDayEl = $("#currentDay")
 
-console.log(currentDayEl)
+var hourEl = $(".hour")
+
 var todayHeader = moment().format("dddd, MMMM YYYY")
 
+var currentTime = moment().format("h A")
+
+
 currentDayEl.text(todayHeader)
+
