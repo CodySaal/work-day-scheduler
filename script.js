6a223708240b4732748d0a1e8ce74fce099e7fe0
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-02 12:57:05 -0500

    Set up local storage, save alert, and added a clear schedule button

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var hourEl = $(".hour")
 var testEl = $(".testClass")
 var timeReference = $(".timeReference")
 var timeBlock = $(".time-block")
+var saveBtn = $(".saveBtn")
+var saveAlert = $(".eventSaved")
+var clearBtn = $(".clearSchedule")
+
 var timeArray = timeReference.text().trim().split(" ")
 var numberTimeArray = timeArray.map(Number);
 var description = $(".description")
@@ -12,6 +16,8 @@ var description = $(".description")
 var todayHeader = moment().format("dddd, MMMM YYYY")
 var currentTimeString = moment().format("HH")
 var currentTime = parseInt(currentTimeString)
+
+saveAlert.hide()
 currentDayEl.text(todayHeader)
 
 function determineTense(){
@@ -32,11 +38,41 @@ function determineTense(){
             $(this).removeClass("future");
             $(this).removeClass("present");
         }
-        console.log(eventTime)
-        console.log(currentTime)
     })
 
 }
+
+saveBtn.on("click", function(){
+    var eventText = $(this).siblings(".description").val();
+    var eventTime = $(this).siblings(".description").attr("id");
+    console.log(eventTime)
+    localStorage.setItem(eventTime, eventText);
+    saveAlert.show().fadeOut(1500)
+})
+
+clearBtn.on("click", function(){
+    localStorage.clear();
+    window.location.reload()
+})
+
+
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
+$("#17").val(localStorage.getItem("17"));
+
+
+
+
+
+
+
+
 
 determineTense();
 
@@ -51,18 +87,3 @@ determineTense();
 
 
 
-
-
-
-
-
-// An idea change back to hourEl This works individually
-
-// function test2(){
-//     if(currentTime.split(" ").join("") === testEl.text().split(" ").join("").trim()){
-//         testEl.addClass("present")
-//     } else{
-//         testEl.addClass("past")
-//     }
-// }
-// test2();
