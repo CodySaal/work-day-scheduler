1275324169631ac16531c1a82bab2ccee0a7eef2
Cody <110630101+CodySaal@users.noreply.github.com>
2022-10-02 00:01:47 -0500

    Added save icons

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
 var currentDayEl = $("#currentDay")
-
 var hourEl = $(".hour")
-
 var testEl = $(".testClass")
+var timeReference = $(".timeReference")
+var timeArray = timeReference.text().trim().split(" ")
+var numberTimeArray = timeArray.map(Number);
+var description = $(".description")
+
+var eventTime = 0
+
+console.log(timeArray)
 
 var todayHeader = moment().format("dddd, MMMM YYYY")
-
 var currentTime = moment().format("h A")
+var military = moment().format("H")
+// var militaryNumber = parseInt(military)
+var militaryNumber = 11
+var militaryEvening = militaryNumber - 12
 
 
 currentDayEl.text(todayHeader)
 
-// function test(){
-//     if (currentTime == testEl.text()){
-//         testEl.addClass("present")
-//     } else{
-//         testEl.addClass("past")
-//     }
-// }
-// console.log(testEl.text())
-// console.log(hourEl.text())
-// console.log(currentTime)
-// test();
+console.log(military)
+console.log(militaryNumber)
+console.log(militaryEvening)
+
+function timeConverter(){
+    if (militaryNumber > 12){
+        militaryNumber -= 12
+    }
+}
+
+function checkTense(){
+    timeConverter();
+    
+    console.log(militaryNumber)
+    for (var i = 0; i < numberTimeArray.length; i++){
+        eventTime = numberTimeArray[i]
+        if(militaryNumber < eventTime){
+            description.removeClass("present")
+            description.removeClass("past")
+            description.addClass("future")
+        } else if (militaryNumber > eventTime){
+            description.removeClass("present")
+            description.removeClass("future")
+            description.addClass("past")
+        } else {
+            description.removeClass("future")
+            description.removeClass("past")
+            description.addClass("present")
+        }
+    }
+}
+checkTense();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -36,27 +82,3 @@ currentDayEl.text(todayHeader)
 //     }
 // }
 // test2();
-
-console.log(currentTime.split(" ").join(""))
-// Ask Josh about this.
-console.log(hourEl.text().split(" ").join(""))
-console.log(testEl.text().split(" ").join(""))
-console.log(testEl.text().split(" ").join("").trim())
-console.log(currentTime.split(" ").join(""))
-
-console.log(currentTime.trim())
-console.log(testEl.text().trim())
-
-function test3(){
-    if(currentTime === hourEl.text().trim()){
-        hourEl.addClass("present")
-    } else {
-        hourEl.addClass("past")
-    }
-}
-test3();
-console.log(hourEl.text().split(" ").join("").trim())
-
-var hourArray = hourEl.text().split(/\r?\n/)
-console.log(hourArray)
-// Maybe split at '?
